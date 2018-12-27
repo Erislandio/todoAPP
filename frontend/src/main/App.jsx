de568a55d37366ae86f38f8b6712ee5b46a37b5f
Erislandio Soares <erislandiosoares21@gmail.com>
2018-12-26 22:11:46 -0200

    Configuração do componente app.jsx

---
+++ frontend/src/main/App.jsx
@@ -0,0 +1,16 @@
+import 'modules/bootstrap/dist/css/bootstrap.min.css'
+import 'modules/font-awesome/css/font-awesome.min.css'
+
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="container">
+                <h1>Teste</h1>
+            </div>
+        );
+    }
+}
+
+export default App;
