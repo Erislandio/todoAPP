dac53c2a20def5ff6c9d191d32ba5cbf1629a627
Erislandio Soares <erislandiosoares21@gmail.com>
2019-01-06 21:27:18 -0200

    Adição do icone de button

--- frontend/src/todo/TodoForm.jsx
+++ frontend/src/todo/TodoForm.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
+import Grid from '../template/Grid';
+import IconButton from '../template/IconButton';
 
 export default props => (
     <div role="form" className="todoForm">
-        <div className="col-xs-12 col-sm-9 col-md-10">
-            <input 
-            type="text" 
-            className="form-control" 
-            id="description"
-            placeholder="Adicione uma tarefa"/>
-        </div>
-        <div className="col-xs-12 col-sm-3 col-md-2">
-            <button className="btn btn-primary">
-                <i className="fa fa-plus"></i>
-            </button>
-        </div>
+        <Grid cols="12 9 10">
+            <input
+                type="text"
+                className="form-control"
+                id="description"
+                placeholder="Adicione uma tarefa" />
+        </Grid>
+
+        <Grid cols="12 3 2">
+           <IconButton style="primary" icon="plus">
+           </IconButton>
+        </Grid>
     </div>
 )
