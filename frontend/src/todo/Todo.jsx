fbffbcdc25fc40cbc9a13eda3c7fbbd789d78907
Erislandio Soares <erislandiosoares21@gmail.com>
2019-01-06 21:09:24 -0200

    Adição do comonente de descrição

--- frontend/src/todo/Todo.jsx
+++ frontend/src/todo/Todo.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 import PageHeader from '../template/PageHeader'
-
+import TodoForm from './TodoForm'
 export default class Todo extends Component {
   render() {
     return (
       <div>
         <PageHeader name="Tarefas" small="Cadastro" />
+        <TodoForm />
       </div>
     )
   }
